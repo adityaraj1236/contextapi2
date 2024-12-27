489c0ba5a46c4684975547bb6cce46ccae3eefdf
Your Name <adityaraj642003@gmail.com>
2024-12-27 23:59:42 +0530

    Context api 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import Login from './components/Login'
-import Profile from './components/Profile'
-import UserContextProvider from './context/UserContextProvider'
+import React, { useEffect } from 'react'
+import { ThemeProvider } from './contexts/Theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 const App = () => {
+  const [themeMode ,  setThemeMode] = React.useState('light')
+
+  const lightTheme = () => setThemeMode('light')
+
+  const darkTheme = () => setThemeMode('dark')
+
+  //actual change in theme 
+  useEffect(()=>{
+      document.querySelector('html').classList.remove('dark' , 'light')
+      document.querySelector('html').classList.add(themeMode)
+  } , [themeMode])
+
   return (
-    <UserContextProvider>
-    <div>
-      <h1 className='bg-red-500'>React with Aditya</h1>
-      <Login/>
-      <Profile/>
-      
+    <ThemeProvider value={{themeMode , lightTheme , darkTheme }}>
+        <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* Theme Button Component */}
+          <ThemeBtn/>
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">{/* Card Component */}
+        <Card/>
+        </div>
+
+      </div>
     </div>
-    </UserContextProvider>
-  )
+    </ThemeProvider>
+
+  );
 }
 
 export default App
